ad9bfdd02c85308dbf8355fec91027e830477fe8
DDRW2005 <dawidwrona@yahoo.co.uk>
2024-11-01 08:40:47 +0100

    scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Dziękujemy za wiadomość! Skontaktujemy się z Tobą wkrótce.");
+});
